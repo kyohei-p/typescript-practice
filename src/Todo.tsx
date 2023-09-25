1ae8c173bea331273ccf16c38d44a0fa198d1207
kyohei-p <107188352+kyohei-p@users.noreply.github.com>
2023-09-25 22:04:00 +0900

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,10 @@
+import { TodoType } from "./types/todo";
+
+export const Todo = (
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+  props: Omit<TodoType, "id">
+) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
+};
